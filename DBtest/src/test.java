4cd2b0052dbbe8b8499d8123aec89a9b0f8f9385
andros230 <andros230@126.com>
2016-06-14 21:47:45 +0800

    test2

--- DBtest/src/test.java
+++ DBtest/src/test.java
@@ -1,10 +1,11 @@
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
 public class test {
 	public static void main(String[] args) throws SQLException {
 		System.out.println(new DB_Kit().select("select * from location where mac !='a0:86:c6:33:e0:12'"));
+		
+		System.out.println("");
 	}
 
 }
