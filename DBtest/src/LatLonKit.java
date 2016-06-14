c975f96795456f4e7a560007e0a0c1c3838fd60b
andros230 <andros230@126.com>
2016-06-14 20:45:08 +0800

    test

---
+++ DBtest/src/LatLonKit.java
@@ -0,0 +1,31 @@
+
+public class LatLonKit {
+    private String mac;
+    private String longitude;
+    private String latitude;
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+}
